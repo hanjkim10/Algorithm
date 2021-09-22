42ac43a0122c779d527179564399f338ad264465
hanjkim10 <hanj.kim.10@gmail.com>
2021-09-23 01:58:24 +0900

    직사각형 별찍기, N개의 최소공배수

--- 프로그래머스 x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스 x만큼 간격이 있는 n개의 숫자.py
@@ -1,9 +1,6 @@
-import sys
-
-x, n = map(int, sys.stdin.readline().split())
-
-result = []
-
-for i in range(1, n+1):
-    result.append(x * i)
-return result
+def solution(x, n):
+    result = []
+    
+    for i in range(1, n+1):
+        result.append(x * i)
+    return result
