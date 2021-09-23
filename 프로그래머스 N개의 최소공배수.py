6b868400db980ab4127d7497412682f4b808c38b
hanjkim10 <hanj.kim.10@gmail.com>
2021-09-23 17:23:29 +0900

    신규아이디 추천

--- 프로그래머스 N개의 최소공배수.py
+++ 프로그래머스 N개의 최소공배수.py
@@ -1,16 +1,11 @@
-arr = [2, 6, 8, 14]
-
-# def solution(arr):
-a = arr.sort(reverse=True)
-
-print(a)
-# for i in range(len(arr)-1):
-#     a = arr[i]
-#     b = arr[i+1]
-#     while a%b:
-#         r = a%b
-#         a = b
-#         b = r
-#     arr[i+1] = (arr[i]*arr[i+1])/b
-
-# return arr[-1]
+def solution(arr):
+    arr.sort(reverse=True)
+    for i in range(len(arr)-1):
+        a = arr[i]
+        b = arr[i+1]
+        while a%b:
+            r = a%b
+            a = b
+            b = r
+        arr[i+1] = (arr[i]*arr[i+1])/b
+    return arr[-1]
