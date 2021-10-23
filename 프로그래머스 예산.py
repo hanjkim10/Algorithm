0402d953ec3a1583280762ccdec1be0066789296
hanjkim10 <hanj.kim.10@gmail.com>
2021-10-23 15:44:12 +0900

    프로그래머스 예산

--- 프로그래머스 예산.py
+++ 프로그래머스 예산.py
@@ -1,2 +1,10 @@
 ## 프로그래머스 예산
 
+def solution(d, budget):
+    d.sort()
+    answer = 0
+    for i in d:
+        if budget - i >= 0:
+            budget -= i
+            answer += 1
+    return answer
