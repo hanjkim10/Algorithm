66177733e72018c811412d5ad54d78887afeaecd
hanjkim10 <hanj.kim.10@gmail.com>
2021-11-04 17:48:01 +0900

    프로그래머스 행렬의 덧셈 수정

--- 프로그래머스 행렬의 덧셈.py
+++ 프로그래머스 행렬의 덧셈.py
@@ -11,4 +11,8 @@ def solution(arr1, arr2):
             
     return answer
 
-    
+def sumMatrix(A,B):
+    for i in range(len(A)) :
+        for j in range(len(A[0])):
+            A[i][j] += B[i][j] 
+    return A
