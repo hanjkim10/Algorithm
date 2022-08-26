04218df2576682137f4f68880c1caddc9bcd4925
hanjkim10 <hanj.kim.10@gmail.com>
2022-08-26 18:18:26 +0900

    11653

--- BaekJoon/baekJoon_11653.py
+++ BaekJoon/baekJoon_11653.py
@@ -1,9 +1,19 @@
-n = int(input())
-m = 2
+N = int(input())
+d = 2
 
-while n != 1:
-    if n & m == 0:
-        print(m)
-        n = n // m
+while N != 1:
+    if N % d != 0:
+        d += 1
     else:
-        m += 1
+        N //= d
+        print(d)
+
+        
+n = int(input())
+i = 2
+while n!= 1:
+    if n % i == 0:
+        n = n / i
+        print(i)
+    else: 
+        i += 1
